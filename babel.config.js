4799544c3afe8d5bc9aa34e1f2698fdf96ea7d23
Ian Cargill <ianc@zillowgroup.com>
2023-03-31 18:28:10 -0700

    focus trap setup

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.expors = {
-  presets: [
-    '@babel/preset-react',
-    ['@babel/preset-env', {targets: { node: 'current'}}],
-    '@babel/preset-typescript',
-    '@babel/plugin-syntax-jsx'
-  ],
-};
